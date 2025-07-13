3a690befd8289c41b7f87eea8d5f5ffb178359ea
barbapapazes <e.soubiran25@gmail.com>
2025-07-13 20:19:44 +0200

    feat: add ui pro with reverb

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+
+window.Echo.channel('test-channel')
+    .listen('TestEvent', (e) => {
+        console.log(e);
+    });
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+// import ui from '@nuxt/ui/vite'
+import uiPro from '@nuxt/ui-pro/vite';
 
 export default defineConfig({
     plugins: [
